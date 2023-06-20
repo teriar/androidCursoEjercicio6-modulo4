a1168b345f4f51544787326c8cf648665148f5d8
Felipe Gonzalez <felipegonzalezurr@gmail.com>
2023-06-19 22:15:43 -0400

    commit con funcionalidad basica

--- app/src/main/java/com/example/cl/ejercicio6java_android/MainActivity.java
+++ app/src/main/java/com/example/cl/ejercicio6java_android/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.cl.ejercicio6java_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText textName = findViewById(R.id.textName);
+        EditText textLastName = findViewById(R.id.textLastName);
+        EditText textEmail = findViewById(R.id.editTextEmail);
+        EditText textPassword = findViewById(R.id.editTextPassword);
+
+        Button btnCreated = findViewById(R.id.btnCreated);
+
+        btnCreated.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(textName.getText().toString().isEmpty() || textLastName.getText().toString().isEmpty() || textEmail.getText().toString().isEmpty()
+                || textPassword.getText().toString().isEmpty() ){
+                    Toast.makeText(getBaseContext(), "faltan campos por completar", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String name = textName.getText().toString();
+                String lastName = textLastName.getText().toString();
+                String email = textEmail.getText().toString();
+                String password = textPassword.getText().toString();
+                String mensaje = "Nombre: " + name + " Apellido: "+ lastName + " Email: " + email + " Password" + password;
+                Toast.makeText(getBaseContext(), mensaje, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
